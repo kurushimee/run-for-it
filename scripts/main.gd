8ad05b388dd9ea340c3001841a7bfc1719ac4d2a
kurushimee <jermacoff@gmail.com>
2024-07-06 23:18:47 +0700

    Fix some bugs

--- scripts/main.gd
+++ scripts/main.gd
@@ -14,8 +14,9 @@ var high_score: int
 
 # Player moving speed
 var speed: float
-const START_SPEED := 10.0
-const MAX_SPEED := 25.0
+const START_SPEED := 12.0
+const MAX_SPEED := 20.0
+const PROSTO_SPEED := 100
 const SPEED_MODIFIER := 5000
 
 var screen_size: Vector2i
@@ -25,17 +26,20 @@ var difficulty: int
 const MAX_DIFFICULTY := 2
 
 var last_obstacle: Area2D
+
 var ground_height: int
+var ground_scale: int
 
 
 func _ready() -> void:
 	screen_size = get_window().size
 	ground_height = $Ground/Sprite2D.texture.get_height()
+	ground_scale = $Ground/Sprite2D.scale.y
 	$GameOver/Button.pressed.connect(new_game)
 	new_game()
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if not game_running:
 		if Input.is_action_pressed("jump"):
 			game_running = true
@@ -43,7 +47,9 @@ func _process(_delta: float) -> void:
 		else:
 			return
 
-	speed = clamp(START_SPEED + score / SPEED_MODIFIER, START_SPEED, MAX_SPEED)
+	speed = (
+		clamp(START_SPEED + score / SPEED_MODIFIER, START_SPEED, MAX_SPEED) * PROSTO_SPEED * delta
+	)
 	adjust_difficulty()
 
 	generate_obstacle()
@@ -105,9 +111,10 @@ func check_high_score() -> void:
 
 
 func generate_obstacle() -> void:
-	if not obstacles.is_empty() or last_obstacle:
-		if last_obstacle.position.x >= score + randi_range(300, 500):
-			return
+	if not obstacles.is_empty():
+		if last_obstacle != null:
+			if last_obstacle.position.x >= score + randi_range(300, 500):
+				return
 	var obstacle_type := available_obstacles[randi() % available_obstacles.size()]
 	var obstacle: Area2D
 
@@ -119,9 +126,14 @@ func generate_obstacle() -> void:
 		var obstacle_height: int = obstacle.get_node("Sprite2D").texture.get_height()
 		var obstacle_scale: Vector2i = obstacle.get_node("Sprite2D").scale
 		# Calculate the position accordingly
-		var obstacle_x: int = screen_size.x + score + 100 + (i * 100)
+		var obstacle_x: int = (
+			screen_size.x + $Player.position.x + (i * 100) + (speed / MAX_SPEED) * 100
+		)
 		var obstacle_y: int = (
-			screen_size.y - ground_height - (obstacle_height * obstacle_scale.y / 2) + 5
+			screen_size.y
+			- (ground_height * ground_scale)
+			- (obstacle_height * obstacle_scale.y / 2)
+			+ (2 * obstacle_scale.y)
 		)
 
 		add_obstacle(obstacle, obstacle_x, obstacle_y)
